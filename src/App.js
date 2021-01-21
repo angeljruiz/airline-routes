aafe9bd5ab388147822918d34c057c0b21d38b4f
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-01-20 22:24:10 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import data from './data'
+
 const App = () => (
   <div className="app">
   <header className="header">
@@ -10,6 +12,22 @@ const App = () => (
     <p>
       Welcome to the app!
     </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Airline</th>
+          <th>Source</th>
+          <th>Destination</th>
+        </tr>
+        { data.routes.map( route => (<>
+          <tr>
+            <td>{route.airline}</td>
+            <td>{route.src}</td>
+            <td>{route.dest}</td>
+          </tr>
+        </>))}
+      </thead>
+    </table>
   </section>
 </div>
 )
